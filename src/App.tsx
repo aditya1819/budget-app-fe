674c0ba5f182e992e0ff226a27087ba2ecfa535b
Aditya Shahare <shahare.aditya@gmail.com>
2023-05-07 18:12:08 +0530

    Added basic UI components using bootstarp

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import { Button, Container, Stack } from 'react-bootstrap';
+import BudgetCard from './Components/BudgetCard';
+
 function App() {
   return (
-    <div className="App">
-      <h1>React budget app</h1>
-    </div>
+    <Container>
+      <Stack direction="horizontal" className="mb-4" gap={2}>
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="primary">Add budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
+          gap: '1rem',
+          alignItems: 'flex-start'
+        }}
+      >
+        <BudgetCard name="budget" amount={20} max={10} gray></BudgetCard>
+      </div>
+    </Container>
   );
 }
 
---
+++ src/Components/BudgetCard.tsx
@@ -0,0 +1,51 @@
+import { Card, ProgressBar, Stack, Button } from 'react-bootstrap';
+import { currencyFormatter } from '../Utils';
+
+type Props = {
+  name: string;
+  amount: number;
+  max: number;
+  gray: boolean;
+};
+
+function getProgressBarVarient(amount: number, max: number) {
+  const ratio = amount / max;
+  return ratio < 0.5 ? 'primary' : ratio < 0.75 ? 'warning' : 'danger';
+}
+
+function BudgetCard({ name, amount, max, gray }: Props) {
+  const classNames = [];
+  if (amount > max) {
+    classNames.push('bg-danger', 'bg-opacity-10');
+  } else if (gray) {
+    classNames.push('bg-light');
+  }
+
+  return (
+    <Card className={classNames.join(' ')}>
+      <Card.Body>
+        <Card.Title className="d-flex justify-content-between align-items-baseline fw-normal mb-3">
+          <div className="me-2">{name}</div>
+          <div>
+            {currencyFormatter.format(amount)} / {currencyFormatter.format(max)}
+          </div>
+        </Card.Title>
+        <ProgressBar
+          className="rounded-pill"
+          variant={getProgressBarVarient(amount, max)}
+          min={0}
+          max={max}
+          now={amount}
+        ></ProgressBar>
+        <Stack direction="horizontal" gap={2} className="mt-4">
+          <Button variant="outline-primary" className="ms-auto">
+            Add Expense
+          </Button>
+          <Button variant="outline-secondary">View Expenses</Button>
+        </Stack>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default BudgetCard;
